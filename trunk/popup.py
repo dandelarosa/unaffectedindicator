4911fa130247c48d9f00a41bd2eb3a148a2123e5
nottingham.dan <nottingham.dan@3990be3b-3d56-0410-b6e3-7555fdba7a8d>
2008-09-10 20:58:52 +0000

    Moving a few more files

--- trunk/popup.py
+++ trunk/popup.py
@@ -4,23 +4,23 @@ from pygame.locals import *
 
 
 class Popup(pygame.sprite.Sprite):
-        def __init__(self):
-                pygame.sprite.Sprite.__init__(self)
-                self.image, self.rect = load_image('popup.bmp', -1)
-                screen = pygame.display.get_surface()
-                self.area = screen.get_rect()
-                self.rect.topleft = random.randint(10, 450), random.randint(10, 280)
-                self.lifetime = 1
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_image('images/popup.bmp', -1)
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+        self.rect.topleft = random.randint(10, 450), random.randint(10, 280)
+        self.lifetime = 1
 		
-        def update(self):
-                self.lifetime+=1
-               # if (self.lifetime < 30):
-                        #need to still display pop up animation
-                  #      print "popping up"
+    def update(self):
+        self.lifetime+=1
+       # if (self.lifetime < 30):
+            #need to still display pop up animation
+          #      print "popping up"
 
 
-        def destroy(self):
-                del self
+    def destroy(self):
+        del self
                 
                 
                 

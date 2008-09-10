7c7c2d4bdf8d8f8bcdd99062926d634843cbf62b
dandelarosa <dandelarosa@3990be3b-3d56-0410-b6e3-7555fdba7a8d>
2008-09-10 20:47:14 +0000

    Added move and delete-link functionality to the tentacle class

--- trunk/boss_test.py
+++ trunk/boss_test.py
@@ -7,49 +7,55 @@ from constants import *
 
 def main():
 
-	pygame.init()
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), False) #pygame.FULLSCREEN)
-	pygame.display.set_caption('Shmup!')
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), False) #pygame.FULLSCREEN)
+    pygame.display.set_caption('Shmup!')
 
-	screen.fill((0, 0, 0))
-	pygame.display.flip()
-	
-	masterclock = pygame.time.Clock()
-	
-	# Create chain (pending implementation)
-	# Tentacle is created with origin at (100,100) with 10 chain links attached
-	tentacle = Tentacle((100,100),10)
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
 
-	# Main game loop
-	gameover = False
-	while gameover == False:
-		masterclock.tick(30)
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				return
-			elif event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_ESCAPE:
-					return
-				elif event.key == pygame.K_UP:
-					tentacle.init_motion((0,-10))
-				elif event.key == pygame.K_DOWN:
-					tentacle.init_motion((0,10))
-				elif event.key == pygame.K_LEFT:
-					tentacle.init_motion((-10,0))
-				elif event.key == pygame.K_RIGHT:
-					tentacle.init_motion((10,0))
+    masterclock = pygame.time.Clock()
 
-		#update stuff
-		tentacle.update()
-		
-		#draw stuff
-		screen.fill((0, 0, 0))
-		
-		# this function call displays the tentacle
-		for link in tentacle.links:
-			screen.blit(link.image,link.rect)
-		
-		# display everything on screen
-		pygame.display.flip() 
+    # Create chain (pending implementation)
+    # Tentacle is created with origin at (100,100) with 10 chain links attached
+    tentacles = [ Tentacle((250,300),10), Tentacle((350,300),10)]
+    to_pull = (0,0)
+
+    # Main game loop
+    gameover = False
+    while gameover == False:
+        masterclock.tick(30)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return
+                elif event.key == pygame.K_SPACE:
+                    for tentacle in tentacles:
+                        tentacle.destroy_link()
+                elif event.key == pygame.K_a:
+                    tentacles[0].destroy_link()
+                elif event.key == pygame.K_s:
+                    tentacles[1].destroy_link()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                to_pull = ( event.pos[0]-tentacle.x, event.pos[1]-tentacle.y )
+                for tentacle in tentacles:
+                    tentacle.init_extend(to_pull)
+
+        #update stuff
+        for tentacle in tentacles:
+            tentacle.update()
+
+        #draw stuff
+        screen.fill((0, 0, 0))
+
+        # this function call displays the tentacle
+        for tentacle in tentacles:
+            for link in tentacle.links:
+                screen.blit(link.image,link.rect)
+
+        # display everything on screen
+        pygame.display.flip() 
 
 main()

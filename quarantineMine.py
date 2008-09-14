f229d50a63c385566819dd79a4210feccb5ba35c
darnisock <darnisock@3990be3b-3d56-0410-b6e3-7555fdba7a8d>
2008-09-14 14:04:16 +0000

    mines rev 3

--- quarantineMine.py
+++ quarantineMine.py
@@ -5,13 +5,15 @@ import entity
 from pygame.locals import *
 
 class QuarantineMine(entity.Entity):
-	def __init__(self,pos):
-                print "mine set"
+	def __init__(self,player,pos):
+                self.player = player
                 super(QuarantineMine, self).__init__(pos, None)
-		t = threading.Timer(5.0, self.explode())
+		t = threading.Timer(5.0, self.explode)
+		t.start()
 		
 	def set_position(self,pos):
 		entity.Entity.set_position(self,pos)
 
 	def explode(self):
+                self.player.quarantine_explode()
                 print "boom"

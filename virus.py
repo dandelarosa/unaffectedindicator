3d795453a706bde222333295f57fab279480aacc
nottingham.dan <nottingham.dan@3990be3b-3d56-0410-b6e3-7555fdba7a8d>
2008-09-14 18:29:41 +0000

    Cleaning stuff up, removing unused file and code

--- virus.py
+++ virus.py
@@ -10,18 +10,13 @@ class Virus(entity.Entity):
         position = random.randint(10,450), 10
         
         super(Virus, self).__init__(position, 'virus.bmp')
-        self.lifetime = 1
-        self.move = 5
+        
+        self.movex = 0
+        self.movey = 4
         
 
     def update(self):
         super(Virus, self).update()
-       # self._fall()
-        self.lifetime+=1
-	
-    def _fall(self):
-		#move the virus down
-        newpos = self.rect.move((0,self.move))
-        self.rect = newpos
+
 	
 	

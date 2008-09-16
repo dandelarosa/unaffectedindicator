026f9cddf9d5c6e700fea1e0f091ca56d676765b
nottingham.dan <nottingham.dan@3990be3b-3d56-0410-b6e3-7555fdba7a8d>
2008-09-16 09:29:38 +0000

    Renaming boss1 to boss, couple quick fixes.

--- constants.py
+++ constants.py
@@ -12,7 +12,6 @@ LIVES = 3
 MAX_HEALTH = 15
 
 PLAY_BG_COLOR = (255, 255, 255)
-HUD_BG_COLOR = (180, 180, 180)
 HUD_BG_COLOR = (230, 230, 230)
 
 BACKGROUND_IMAGE_FILES = ('disc.png', 'folder.png', 'game icon.png', 'game icon 2.png', 'monkey exe.png', 'mp3.png', 'my computer.png', 'notepad.png', 'world icon.png')

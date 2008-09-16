019afccfb5bb24a762cdb322ce4119b2d49aafd1
nottingham.dan <nottingham.dan@3990be3b-3d56-0410-b6e3-7555fdba7a8d>
2008-09-16 23:13:50 +0000

    - Resize animations for popups spawning and dying. - Fixing damage bug and such.

--- trunk/animation.py
+++ trunk/animation.py
@@ -1,11 +1,19 @@
 import pygame
 
+def loadImage (image):
+    if (isinstance(image, str)):
+        return pygame.image.load('data/images/' + image).convert_alpha()
+    else:
+        assert(isinstance(image, pygame.Surface))
+        return image.convert_alpha()
+    
+
 class Animation (object):
 
-    def __init__(self, imageFile = None, frameWidth = 0, frameRate = 1, loops = True):
+    def __init__(self, image = None, frameWidth = 0, frameRate = 1, loops = True):
         super(Animation, self).__init__()
         
-        self.loadAnimation(imageFile, frameWidth)
+        self.loadAnimation(image, frameWidth)
         
         self.frame = 0
         self.animFrame = 0
@@ -34,8 +42,8 @@ class Animation (object):
         self.animRect = self.animRects[0]
         self.done = False
     
-    def loadAnimation(self, imageFilename = None, frameWidth = 0):
-        if imageFilename is None:
+    def loadAnimation(self, image = None, frameWidth = 0):
+        if image is None:
             self.image = pygame.Surface((32, 32))
             color = (100, 100, 100)
             self.image.fill(color)
@@ -43,7 +51,7 @@ class Animation (object):
             self.animRects = [self.rect]
             
         else:
-            self.image = pygame.image.load('data/images/' + imageFilename).convert_alpha()
+            self.image = loadImage(image)
             rectStrip = self.image.get_rect()
             
             if frameWidth == 0:
@@ -56,4 +64,20 @@ class Animation (object):
             self.animRect = self.animRects[0]
             self.rect = pygame.Rect(0, 0, self.animRect.width, self.animRect.height)
             
-
+class ResizeAnimation(Animation):
+    def __init__(self, image, numFrames, initial, final):
+        image = loadImage(image)
+        rect = image.get_rect()
+        imageStrip = pygame.Surface((rect.width * numFrames, rect.height)).convert_alpha()
+        imageStrip.fill((0,0,0,0))
+        image = image.convert_alpha(imageStrip)
+        
+        for i in range(numFrames):
+            scale = ((final[0] - initial[0]) * (float(i)/numFrames) + initial[0], (final[1] - initial[1]) * (float(i)/numFrames) + initial[1])
+            scale = (scale[0] * rect.width, scale[1] * rect.height)
+            imageStrip.blit(pygame.transform.scale(image, scale), (rect.width * i, 0))
+        
+        super(ResizeAnimation, self).__init__(imageStrip, rect.width, 1, False)
+        
+        
+        

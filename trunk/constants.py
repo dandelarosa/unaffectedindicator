0937231fadbbe9ee54392c30b776a019849b002f
nottingham.dan <nottingham.dan@3990be3b-3d56-0410-b6e3-7555fdba7a8d>
2008-09-16 20:55:35 +0000

    Changing music timing, taking "monkey exe.png" out of backgrounds

--- trunk/constants.py
+++ trunk/constants.py
@@ -3,9 +3,15 @@ HUD_WIDTH = 200
 SCREEN_WIDTH = PLAY_WIDTH + HUD_WIDTH
 SCREEN_HEIGHT = 768
 
-#play through Main.wav and Mainloop.wav once each, then boss shows up
-#length = 2:20 + 1:30 = 230 seconds = 6900 frames
-FRAMES_UNTIL_BOSS = 6900
+#play through Main.mp3 and Mainloop.mp3 once each, then boss shows up
+#length = 2:48 + 1:48 = 276 seconds = 8280 frames
+FRAMES_UNTIL_BOSS = 8280
+
+#or just play Main.mp3 once to keep things shorter
+#length = 2:48 = 168 seconds = 5040 frames
+#FRAMES_UNTIL_BOSS = 5040
+
+#uncomment this to just test boss
 #FRAMES_UNTIL_BOSS = 1
 
 LIVES = 3
@@ -14,5 +20,5 @@ MAX_HEALTH = 15
 PLAY_BG_COLOR = (255, 255, 255)
 HUD_BG_COLOR = (230, 230, 230)
 
-BACKGROUND_IMAGE_FILES = ('disc.png', 'folder.png', 'game icon.png', 'game icon 2.png', 'monkey exe.png', 'mp3.png', 'my computer.png', 'notepad.png', 'world icon.png')
+BACKGROUND_IMAGE_FILES = ('disc.png', 'folder.png', 'game icon.png', 'game icon 2.png', 'mp3.png', 'my computer.png', 'notepad.png', 'world icon.png')
 
